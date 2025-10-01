526d6dc327c4958e65318cffbbf12e8c47e52ac8
Gionathan Vargas <gioanathanv@gmail.com>
2025-10-01 18:55:07 -0400

    changed folder name

--- src/app/api/auth/[kindeAuth]/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/auth/[...kinde]/route.ts (or wherever you call handleAuth)
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export const runtime = "nodejs";           // 👈 ensure Node runtime
-export const dynamic = "force-dynamic";    // avoid caching
-
-export const GET = handleAuth();
-export const POST = handleAuth();
