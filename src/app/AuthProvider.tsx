86a863252617a350a66925a594d8b9ea163d679a
Gionathan Vargas <gioanathanv@gmail.com>
2025-10-01 19:11:14 -0400

    added authprovider

---
+++ src/app/AuthProvider.tsx
@@ -0,0 +1,8 @@
+// app/AuthProvider.tsx
+"use client";
+
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
+
+export default function AuthProvider({ children }: { children: React.ReactNode }) {
+  return <KindeProvider>{children}</KindeProvider>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,30 +4,28 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { Navbar } from "@/components/Navbar";
 import TanStackProvider from "@/components/providers/TanStackProvider";
-
+import AuthProvider from "./AuthProvider"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Stripe Subscriptions",
-	description: "Learn how to integrate Stripe subscriptions with Next.js",
+  title: "Stripe Subscriptions",
+  description: "Learn how to integrate Stripe subscriptions with Next.js",
 };
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-					<TanStackProvider>
-						<Navbar />
-						{children}
-					</TanStackProvider>
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider> {/* 👈 Kinde at the top */}
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <TanStackProvider>
+              <Navbar />
+              {children}
+            </TanStackProvider>
+          </ThemeProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
 }
